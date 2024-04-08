4e8f53df3d782beb290bc45647c7042ba94a9d6f
vasylyna <vasylyna.misiura@beetroot.se>
2024-04-08 17:46:54 +0300

    Some common scripts are created

---
+++ script.js
@@ -0,0 +1,14 @@
+<script>
+    alert("Привіт, це спливаюче вікно!");
+
+
+
+    document.write("Привіт, світ!");
+
+
+
+    document.getElementById("myElement").style.color = "red";
+
+
+<button onclick="alert('Ви натиснули на кнопку!')">Натисніть мене</button>
+</script>
